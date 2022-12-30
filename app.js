dff4c138ab9a4e4537b95e3766d65bb364cf5066
valeiras <d.r.valeiras@gmail.com>
2022-12-30 17:22:08 +0100

    Without imports

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 //jshint esversion:6
-import "./lib/db";
+import mongoose from "mongoose";
+
+if (!process.env.MONGO_URL) {
+  throw new Error("Please add the MONGO_URL environment variable");
+}
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const database = mongoose.connection;
+
+database.on(
+  "error",
+  console.error.bind(console, "❌ mongodb connection error"),
+);
+database.once("open", () => console.log("✅ mongodb connected successfully"));
+
+mongoose.Promise = Promise;
 
 const express = require("express");
 const bodyParser = require("body-parser");
